b8e211f495fffff62ac61f4e9128a465c8a85aae
adaihao <gyl_adaihao@163.com>
2016-06-06 17:25:08 +0800

    delete test

--- rocketmq-example/src/main/java/com/wish/rocketmq/example/gyl/DelayTime/DelayTimeConsumer.java
+++ rocketmq-example/src/main/java/com/wish/rocketmq/example/gyl/DelayTime/DelayTimeConsumer.java
@@ -1,4 +1,6 @@
-package com.wish.rocketmq.example.ttx.gyl.messageLoss;
+package com.wish.rocketmq.example.gyl.DelayTime;
+
+import java.util.List;
 
 import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
 import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
@@ -8,14 +10,12 @@ import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.common.message.MessageExt;
 import com.alibaba.rocketmq.example.filter.MessageFilterImpl;
 
-import java.util.List;
-
 
 /**
  * @author: GaoYanLei
  * @since: 2016/6/3
  */
-public class MessageLossConsumer {
+public class DelayTimeConsumer {
     public static void main(String[] args) throws InterruptedException, MQClientException {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("messageLoss");
         /**
