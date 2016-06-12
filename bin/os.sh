9a9e6b1e35dc2c409e1ac6abba5db6dc682ef3e6
tantexian <tantexian@qq.com>
2016-06-12 19:51:08 +0800

    1、将测试用例包名更新为com.gome 2、修复rocketmq-example缺少broker包依赖报错

--- bin/os.sh
+++ bin/os.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-#
 # Execute Only Once
-#
+
+#当前程序运行环境用户名
+USERNAME=root
+#需要修改IO模式磁盘盘符
+DISKNAME=sda
 
 echo 'vm.overcommit_memory=1' >> /etc/sysctl.conf
 echo 'vm.min_free_kbytes=5000000' >> /etc/sysctl.conf
@@ -14,14 +17,15 @@ echo 'vm.dirty_ratio=50' >> /etc/sysctl.conf
 echo 'vm.page-cluster=3' >> /etc/sysctl.conf
 echo 'vm.dirty_writeback_centisecs=360000' >> /etc/sysctl.conf
 echo 'vm.swappiness=10' >> /etc/sysctl.conf
+cat /etc/sysctl.conf
 sysctl -p
 
 echo 'ulimit -n 655350' >> /etc/profile
-echo 'admin hard nofile 655350' >> /etc/security/limits.conf
+echo "$USERNAME hard nofile 655350" >> /etc/security/limits.conf
 
-DISK=`df -k | sort -n -r -k 2 | awk -F/ 'NR==1 {gsub(/[0-9].*/,"",$3); print $3}'`
-[ "$DISK" = 'cciss' ] && DISK='cciss!c0d0'
-echo 'deadline' > /sys/block/$DISK/queue/scheduler
+#DISK=`df -k | sort -n -r -k 2 | awk -F/ 'NR==1 {gsub(/[0-9].*/,"",$3); print $3}'`
+#[ "$DISK" = 'cciss' ] && DISK='cciss!c0d0'
+echo 'deadline' > /sys/block/$DISKNAME/queue/scheduler
 
 
 echo "---------------------------------------------------------------"
@@ -36,5 +40,5 @@ sysctl vm.page-cluster
 sysctl vm.dirty_writeback_centisecs
 sysctl vm.swappiness
 
-su - admin -c 'ulimit -n'
-cat /sys/block/$DISK/queue/scheduler
+su - $USERNAME -c 'ulimit -n'
+cat /sys/block/$DISKNAME/queue/scheduler
