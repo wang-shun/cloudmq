5f043e026461ff67cbdad2f77c33f6fffd1234a6
tantexian <tantexian@qq.com>
2016-06-13 21:18:16 +0800

    更新nameservere

--- rocketmq-example/src/main/java/com/gome/rocketmq/example/ttx/MyUtil.java
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/ttx/MyUtil.java
@@ -3,7 +3,7 @@ package com.gome.rocketmq.example.ttx;
 public class MyUtil {
 
     private static String namesrvAddr =
-            "10.128.31.103:9876;10.128.31.104:98;10.128.31.105:9876;10.128.31.106:9876";
+            "10.128.31.103:9876;10.128.31.104:9876;10.128.31.105:9876;10.128.31.106:9876";
 
 
     public static String getNamesrvAddr() {
