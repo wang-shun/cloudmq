a34ce6f69ce8625bc31b5fd3254e41d479819db6
adaihao <gyl_adaihao@163.com>
2016-06-14 16:31:48 +0800

    update namesrvAddr

---
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/gyl/topic/CreateTopicNumTest.java
@@ -0,0 +1,84 @@
+package com.gome.rocketmq.example.gyl.topic;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.client.producer.SendStatus;
+import com.alibaba.rocketmq.common.message.Message;
+import com.gome.rocketmq.common.MyUtil;
+
+
+/**
+ * @author: GaoYanLei
+ * @since: 2016/6/12
+ */
+
+public class CreateTopicNumTest {
+    final static int nThreads = 2;
+    final static int topicNums = 10;
+
+
+    public static void main(String[] args) throws MQClientException {
+        final AtomicLong atomicSuccessNums = new AtomicLong(0l);
+        final DefaultMQProducer producer = new DefaultMQProducer("CreateTopicNumTest");
+        producer.setNamesrvAddr(MyUtil.getNamesrvAddr());
+        producer.start();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final CyclicBarrier barrier = new CyclicBarrier(nThreads, new Runnable() { // 设置几个线程为一组,当这一组的几个线程都执行完成后,然后执行住线程的
+            @Override
+            public void run() {
+                System.out.printf("All message has send, send topicNums is : %d, " + "Success nums is : %d ",
+                    nThreads * topicNums, atomicSuccessNums.get());
+                producer.shutdown();
+            }
+        });
+        for (int i = 0; i < nThreads; i++) {
+            exec.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        for (int j = 0; j < topicNums; j++) {
+                            final Message message =
+                                    new Message("createTopic_" + Thread.currentThread().getName() + "_" + j,
+                                        "A", ("test1" + j).getBytes());
+                            SendResult sendResult = producer.send(message);
+                            if (sendResult.getSendStatus() == SendStatus.SEND_OK) {
+                                // System.out.println(Thread.currentThread().getName()
+                                // + "============"
+                                // + atomicSuccessNums.incrementAndGet() +
+                                // "========"
+                                // + sendResult.getMessageQueue().getQueueId());
+                                System.out.println(atomicSuccessNums.incrementAndGet());
+                            }
+                            else {
+                                System.out.println("#### ERROR Message :" + sendResult);
+                            }
+                        }
+                        System.out.println(
+                            (barrier.getNumberWaiting() + 1) + "位完成：" + Thread.currentThread().getName());
+                        barrier.await();
+                    }
+                    catch (Exception e) {
+                        try {
+                            barrier.await();
+                            System.out.println((Thread.currentThread().getName() + "未完成"));
+                        }
+                        catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                        }
+                        catch (BrokenBarrierException e1) {
+                            e1.printStackTrace();
+                        }
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
