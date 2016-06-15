96130a34448ed71d93300507fa298b121c5fa978
adaihao <gyl_adaihao@163.com>
2016-06-15 17:53:04 +0800

    加入 测试消费者

---
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/gyl/DelayTime/DelayTimeConsumer1.java
@@ -0,0 +1,40 @@
+package com.gome.rocketmq.example.gyl.DelayTime;
+
+import java.util.List;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import com.alibaba.rocketmq.common.protocol.heartbeat.MessageModel;
+import com.gome.rocketmq.common.MyUtil;
+
+
+/**
+ * @author: GaoYanLei
+ * @since: 2016/6/3
+ */
+public class DelayTimeConsumer1 {
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("delayTime");
+        consumer.setNamesrvAddr(MyUtil.getNamesrvAddr());
+        consumer.subscribe("delayTimeTopic", "*");
+//        consumer.setMessageModel(MessageModel.BROADCASTING);
+        consumer.setMessageModel(MessageModel.CLUSTERING);
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                    ConsumeConcurrentlyContext context) {
+                MessageExt msg = msgs.get(0);
+                System.out.println("Consumer2:==============" + Thread.currentThread().getName()
+                        + "==============" + new String(msg.getMsgId()) + "============== 延迟:"
+                        + (System.currentTimeMillis() - msg.getBornTimestamp()) / 1000 + "秒");
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.println("Consumer Started.");
+    }
+}
