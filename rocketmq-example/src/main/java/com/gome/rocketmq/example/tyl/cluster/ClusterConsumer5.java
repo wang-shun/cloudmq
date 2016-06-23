b904a3c961678287f0d777afddea720885b732a0
tianyuliang <tianyuliang@139.com>
2016-06-23 10:19:18 +0800

    add broadcast consumer model

---
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/tyl/cluster/ClusterConsumer5.java
@@ -0,0 +1,44 @@
+package com.gome.rocketmq.example.tyl.cluster;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import com.alibaba.rocketmq.common.protocol.heartbeat.MessageModel;
+import com.gome.rocketmq.common.MyUtils;
+
+import java.util.List;
+
+
+/**
+ * @author: GaoYanLei
+ * @since: 2016/6/3
+ */
+public class ClusterConsumer5 {
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+        try {
+            final DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(MyUtils.getDefaultCluster());
+            consumer.setNamesrvAddr(MyUtils.getNamesrvAddr());
+            consumer.subscribe("orderTopicTest", "*");
+            consumer.setMessageModel(MessageModel.BROADCASTING);
+            consumer.setConsumerGroup("DefaultCluster3");
+            consumer.registerMessageListener(new MessageListenerConcurrently() {
+                @Override
+                public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                    for (MessageExt msg : msgs) {
+                        System.out.println("consumer=" + consumer.getConsumerGroup()
+                                + ", instanceName=" + consumer.getInstanceName() + ", msgId=" + new String(msg.getMsgId())
+                                + ", queueId=" + msg.getQueueId() + ",offset=" + msg.getQueueOffset());
+                    }
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+            });
+            consumer.start();
+            System.out.println("Consumer Started.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/tyl/cluster/ClusterProducer.java
@@ -0,0 +1,50 @@
+package com.gome.rocketmq.example.tyl.cluster;
+
+import com.alibaba.rocketmq.client.exception.MQBrokerException;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.client.producer.SendStatus;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.remoting.exception.RemotingException;
+import com.gome.rocketmq.common.MyUtils;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+/**
+ * @author: GaoYanLei
+ * @since: 2016/6/3
+ */
+public class ClusterProducer {
+    public static void main(String[] args) {
+        try {
+            DefaultMQProducer producer = new DefaultMQProducer(MyUtils.getDefaultCluster());
+            producer.setNamesrvAddr(MyUtils.getNamesrvAddr());
+            producer.start();
+            int sendOneTime = 10;
+            SendResult sendResult = null;
+            Message message = null;
+            for (int i = 0; i < sendOneTime; i++) {
+                message = new Message("orderTopicTest", "tagA", ("data" + i).getBytes());
+                sendResult = producer.send(message);
+                if (sendResult.getSendStatus() == SendStatus.SEND_OK) {
+                    System.out.println("send msg. topic=" + message.getTopic() + ",msgId=" + sendResult.getMsgId() + ",queueId=" + sendResult.getMessageQueue().getQueueId() + ",offset=" + sendResult.getQueueOffset());
+                }
+            }
+            producer.shutdown();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
