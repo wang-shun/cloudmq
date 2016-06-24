8cde66af62f10f61df6fe8e9345f91ef9d836034
tianyuliang <tianyuliang@139.com>
2016-06-24 16:26:28 +0800

    update queue default numbers

---
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/tyl/simple/PullConsumer.java
@@ -0,0 +1,90 @@
+package com.gome.rocketmq.example.tyl.simple;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPullConsumer;
+import com.alibaba.rocketmq.client.consumer.PullCallback;
+import com.alibaba.rocketmq.client.consumer.PullResult;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.MixAll;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import com.alibaba.rocketmq.common.message.MessageQueue;
+import com.gome.rocketmq.common.MyUtils;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.*;
+
+/**
+ * test max pullBatchNumbers
+ *
+ * @author tianyuliang
+ * @date 2016/6/24
+ */
+public class PullConsumer {
+    private static final Map<MessageQueue, Long> offseTable = new HashMap<MessageQueue, Long>();
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPullConsumer consumer = new DefaultMQPullConsumer(MyUtils.getDefaultCluster());
+        consumer.setNamesrvAddr(MyUtils.getNamesrvAddr());
+        consumer.start();
+        System.out.println("pull consumer start.");
+        int maxNums = 31;
+        Set<MessageQueue> mqs = consumer.fetchSubscribeMessageQueues("pullTest");
+        for (MessageQueue mq : mqs) {
+            System.out.println("consume from the queue: " + mq);
+            SINGLE_MQ: while (true) {
+                try {
+                    PullResult pullResult = consumer.pullBlockIfNotFound(mq, null, getMessageQueueOffset(mq), maxNums);
+                    List<MessageExt> msgExtList = pullResult.getMsgFoundList();
+                    if (msgExtList == null){
+                        System.out.println("msgExtList is empty.");
+                        continue ;
+                    }
+                   // System.out.println(pullResult);
+                    Long maxOffset = pullResult.getMaxOffset();
+                    Long msgExtSize = new Long((long) msgExtList.size());
+                    Long nextBeginOffset = pullResult.getNextBeginOffset();
+                    System.out.println("maxOffset=" + maxOffset + ",msgExtSize=" + msgExtSize + ",nextBeginOffset=" + nextBeginOffset + ",maxNums=" + maxNums);
+
+                    putMessageQueueOffset(mq, nextBeginOffset);
+                    //sleepTime(1);
+                    switch (pullResult.getPullStatus()) {
+                        case FOUND:
+                            // TODO
+                            break;
+                        case NO_MATCHED_MSG:
+                            break;
+                        case NO_NEW_MSG:
+                            break SINGLE_MQ;
+                        case OFFSET_ILLEGAL:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        consumer.shutdown();
+    }
+
+
+    private static void putMessageQueueOffset(MessageQueue mq, long offset) {
+        offseTable.put(mq, offset);
+    }
+
+
+    private static long getMessageQueueOffset(MessageQueue mq) {
+        Long offset = offseTable.get(mq);
+        return offset != null ? offset : 0L;
+    }
+
+    private static void sleepTime(int second) {
+        try {
+            Thread.sleep(second * 1000L);
+        } catch (Exception e) {
+            System.out.println("error:" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ rocketmq-example/src/main/java/com/gome/rocketmq/example/tyl/simple/PullProducer.java
@@ -0,0 +1,82 @@
+package com.gome.rocketmq.example.tyl.simple;
+
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.client.producer.SendStatus;
+import com.alibaba.rocketmq.common.message.Message;
+import com.gome.rocketmq.common.MyUtils;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * send default message for pull/push consumer
+ *
+ * @author tianyuliang
+ * @date 2016/6/24
+ */
+public class PullProducer {
+    final static int nThread = 30;
+    final static int sendOneTime = 20000;
+    final static String topicName = "pullTest";
+
+    public static void main(String[] args) throws MQClientException {
+        final AtomicLong success = new AtomicLong(0);
+        final DefaultMQProducer producer = new DefaultMQProducer(MyUtils.getDefaultCluster());
+        producer.setNamesrvAddr(MyUtils.getNamesrvAddr());
+        producer.start();
+
+        final CyclicBarrier barrier = new CyclicBarrier(nThread, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " send message end. " + success.get());
+                producer.shutdown();
+            }
+        });
+
+        ExecutorService excutor = Executors.newFixedThreadPool(nThread);
+        for (int i = 0; i < nThread; i++) {
+            excutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    sendMessage(producer, success, barrier);
+                }
+            });
+        }
+    }
+
+    private static void sendMessage(DefaultMQProducer producer, AtomicLong success, CyclicBarrier barrier ) {
+        try {
+            Message message = null;
+            for (int j = 0; j < sendOneTime; j++) {
+                message = new Message(topicName, "A", "data".getBytes());
+                SendResult result = producer.send(message);
+                if (result.getSendStatus() == SendStatus.SEND_OK) {
+                    System.out.println("send msg success. success=" + success.incrementAndGet() + ",topic=" + message.getTopic()
+                            + ",offset=" + result.getQueueOffset() + ",body=" + new String(message.getBody()) + ",msgId=" + result.getMsgId());
+                } else {
+                    System.out.println("error: " + Thread.currentThread().getName() + "===" + success.get() + "===" + result.toString());
+                }
+            }
+            System.out.println("send message successful. " + success.get());
+            barrier.await();
+        } catch (Exception e) {
+            System.out.println("error: " + Thread.currentThread().getName() + " === " + e.getMessage());
+            try {
+                barrier.await();
+                System.out.println((Thread.currentThread().getName() + "未完成"));
+            }
+            catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            catch (BrokenBarrierException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+}
