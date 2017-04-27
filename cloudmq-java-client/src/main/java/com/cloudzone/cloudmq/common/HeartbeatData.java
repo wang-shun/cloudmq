6282b08e30882cb0d99d9cd633eae6748f3564d4
ytq <261026193@qq.com>
2017-04-27 17:42:21 +0800

    update

--- cloudmq-java-client/src/main/java/com/cloudzone/cloudmq/common/HeartbeatData.java
+++ cloudmq-java-client/src/main/java/com/cloudzone/cloudmq/common/HeartbeatData.java
@@ -1,10 +1,6 @@
 package com.cloudzone.cloudmq.common;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-import com.cloudzone.cloudmq.util.HttpTinyClient;
-
-import static javafx.scene.input.KeyCode.H;
 
 /**
  * @author yintongjiang
